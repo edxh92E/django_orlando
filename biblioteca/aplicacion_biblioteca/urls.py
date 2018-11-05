7102b71839a0c244d775423a5c2cbef994c59462
Daniel Huinac <edxiloj25huinac@gmail.com>
2018-11-04 19:14:34 -0600

    Detalle de libros

--- biblioteca/aplicacion_biblioteca/urls.py
+++ biblioteca/aplicacion_biblioteca/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.libros_list, name='home'),
     path('listado_libros', views.listado_libros, name='listado_libros'),
+    path('detalle_libro/<int:pk>/', views.detalle_libro, name='detalle_libro'),
+
 ]
--- biblioteca/aplicacion_biblioteca/views.py
+++ biblioteca/aplicacion_biblioteca/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Libro
 
@@ -11,3 +11,7 @@ def libros_list(request):
 def listado_libros(request):
     libros = Libro.objects.all()
     return render(request, 'libros/listado_libros.html', {'listado_libros': libros })
+
+def detalle_libro(request, pk):
+    libro = get_object_or_404(Libro, pk=pk)
+    return render(request, 'libros/detalle_libro.html', {'libro': libro})
