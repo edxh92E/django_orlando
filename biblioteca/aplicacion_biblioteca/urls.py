9b01cf3676ed6f6c06188f785560e13d1432bc9c
Daniel Huinac <edxiloj25huinac@gmail.com>
2018-11-04 18:34:37 -0600

    Listado de libros con dise;o de bootstrap 4

--- biblioteca/aplicacion_biblioteca/urls.py
+++ biblioteca/aplicacion_biblioteca/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.libros_list, name='libros_list')
+    path('', views.libros_list, name='home'),
+    path('listado_libros', views.listado_libros, name='listado_libros'),
 ]
--- biblioteca/aplicacion_biblioteca/views.py
+++ biblioteca/aplicacion_biblioteca/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Libro
 
 # Create your views here.
 def libros_list(request):
-    return render(request, 'libros/libros_list.html', {})
+    libros = Libro.objects.all()
+    # print(libros)
+    return render(request, 'libros/libros_list_home.html', {'listado_libros': libros })
+
+def listado_libros(request):
+    libros = Libro.objects.all()
+    return render(request, 'libros/listado_libros.html', {'listado_libros': libros })
