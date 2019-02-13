4199ec672c60297645e2b7edc05114fe414a9584
Daniel Huinac <edxiloj25huinac@gmail.com>
2019-02-12 22:04:02 -0600

    comentario

--- biblioteca/aplicacion_biblioteca/admin.py
+++ biblioteca/aplicacion_biblioteca/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Libro
+from .models import Libro, LibroUsuario, DetallePrestado
 
 # Register your models here.
 admin.site.register(Libro)
+admin.site.register(LibroUsuario)
+admin.site.register(DetallePrestado)
